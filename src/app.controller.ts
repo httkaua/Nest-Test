5275be283bf4f6f76d3bf87f8b1bb47bcc9652b9
kaua_hamilton <kaua04hamilton@gmail.com>
2025-05-07 22:40:07 -0300

    Nest server created

---
+++ src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Req, Next, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+import { get } from 'http';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
